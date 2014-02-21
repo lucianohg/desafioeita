6c8c1032097241696568a080fdd4463f79144152
Luciano Holanda <luciano.hg4@gmail.com>
2014-02-20 21:45:29 -0300

    detalhes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ DesafioEita::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  get "static_pages/home"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
